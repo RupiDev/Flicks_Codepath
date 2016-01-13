aaf0a402fcc051929e52d550539c3c99911372bd
Ravinder Bhalla <ravinder.bhalla@gmail.com>
2016-01-12 19:17:06 -0500

    Initial Commit

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Rupin Bhalla on 1/10/16.
+//  Copyright © 2016 Rupin Bhalla. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var moviePic: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'Flicks' do
+
+pod ‘AFNetworking’, ‘~> 2.6’
+pod ‘EZLoadingActivity’
+
+end
+
+target 'FlicksTests' do
+
+end
+
+target 'FlicksUITests' do
+
+end
+
